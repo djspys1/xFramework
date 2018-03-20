8b2a1a909b0a7f09da8692e5f1e57072c6064007
xiaopeng-PC\xiaopeng <972551702@qq.com>
2018-03-20 22:18:20 +0800

    新增控制器分发组件http-kernel以及优化相关代码

--- src/app.php
+++ src/app.php
@@ -10,24 +10,31 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RouteCollection;
 
-function is_leap_year($year = null) {
-    if (null === $year) {
-        $year = date('Y');
+class LeapYearController
+{
+    public function indexAction($year)
+    {
+        if (is_leap_year($year)) {
+            return new Response('Yep, this is a leap year!!');
+        }
+
+        return new Response('Nope, this is not a leap year!!');
     }
 
-    return 0 === $year % 400 || (0 === $year % 4 && 0 !== $year % 100);
 }
+
+function is_leap_year($year = null) {
+        if (null === $year) {
+            $year = date('Y');
+        }
+
+        return 0 === $year % 400 || (0 === $year % 4 && 0 !== $year % 100);
+    }
 
 $routes = new RouteCollection();
 $routes->add('leap_year', new Route('is_leap_year/{year}', array(
     'year' => null,
-    '_controller' => function ($request) {
-        if (is_leap_year($request->attributes->get('year'))) {
-            return new Response('Yep, this is a leap yar!!');
-        }
-
-        return new Response('Nope, this is not a leap yar!!');
-    }
+    '_controller' => 'LeapYearController::indexAction',
 )));
 
 
--- web/front.php
+++ web/front.php
@@ -8,6 +8,8 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
+use Symfony\Component\HttpKernel\Controller\ControllerResolver;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 use Symfony\Component\Routing\Matcher\UrlMatcher;
 use Symfony\Component\Routing\RequestContext;
@@ -26,9 +28,16 @@ $context = new RequestContext();
 $context->fromRequest($request);
 $matcher = new UrlMatcher($routes, $context);
 
+$controllerResolver = new ControllerResolver();
+$argumentResolver = new ArgumentResolver();
+
 try {
     $request->attributes->add($matcher->match($request->getPathInfo()));
-    $response = call_user_func($request->attributes->get('_controller'), $request);
+
+    $controller = $controllerResolver->getController($request);
+    $arguments = $argumentResolver->getArguments($request, $controller);
+
+    $response = call_user_func_array($controller, $arguments);
 } catch (ResourceNotFoundException $exception) {
     $response = new Response('Not Found!!!', 404);
 } catch (Exception $exception) {
