c7e33532b428064b09b9f23a5202a2ade9e2a044
xiaopeng-PC\xiaopeng <972551702@qq.com>
2018-03-20 21:22:37 +0800

    新增默认值

--- src/app.php
+++ src/app.php
@@ -10,7 +10,7 @@ use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RouteCollection;
 
 $routes = new RouteCollection();
-$routes->add('hello', new Route('/hello/{name}'));
+$routes->add('hello', new Route('/hello/{name}', array('name' => 'world')));
 $routes->add('bye', new Route('bye'));
 
 return $routes;
