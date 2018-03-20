f1a033e6e9bbd7da24f4fc0f83205bd6fda3f2b7
xiaopeng-PC\xiaopeng <972551702@qq.com>
2018-03-20 23:57:16 +0800

    新增单元测试相关组件并新增测试用例

---
+++ tests/Simplex/Tests/FrameworkTest.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xiaopeng
+ * Date: 2018/3/20
+ * Time: 22:21
+ */
+
+namespace Simplex\Tests;
+
+
+use PHPUnit\Framework\TestCase;
+use Simplex\Framework;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
+use Symfony\Component\HttpKernel\Controller\ControllerResolverInterface;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+use Symfony\Component\Routing\Matcher\UrlMatcherInterface;
+use Symfony\Component\Routing\RequestContext;
+
+class FrameworkTest extends TestCase
+{
+    public function testNotFoundHandling()
+    {
+        $framework = $this->getFrameworkForException(new ResourceNotFoundException());
+        $response = $framework->handle(new Request());
+        $this->assertEquals(404, $response->getStatusCode());
+    }
+
+    private function getFrameworkForException($exception)
+    {
+        $matcher = $this->createMock(UrlMatcherInterface::class);
+        $matcher
+            ->expects($this->once())
+            ->method('match')
+            ->will($this->throwException($exception));
+        $matcher
+            ->expects($this->once())
+            ->method('getContext')
+            ->will($this->returnValue($this->createMock(RequestContext::class)));
+        $controllerResolver = $this->createMock(ControllerResolverInterface::class);
+        $arumentResolver = $this->createMock(ArgumentResolver::class);
+
+        return new Framework($matcher, $controllerResolver, $arumentResolver);
+    }
+}
