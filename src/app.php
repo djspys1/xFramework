d1e83dbafcd73f9f95b0bc5376d852390bc362bd
xiaopeng-PC\xiaopeng <972551702@qq.com>
2018-03-20 21:48:34 +0800

    优化模板

--- src/app.php
+++ src/app.php
@@ -6,11 +6,32 @@
  * Time: 20:49
  */
 
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RouteCollection;
 
+function is_leap_year($year = null) {
+    if (null === $year) {
+        $year = date('Y');
+    }
+
+    return 0 === $year % 400 || (0 === $year % 4 && 0 !== $year % 100);
+}
+
 $routes = new RouteCollection();
-$routes->add('hello', new Route('/hello/{name}', array('name' => 'world')));
-$routes->add('bye', new Route('bye'));
+$routes->add('leap_year', new Route('is_leap_year/{year}', array(
+    'year' => null,
+    '_controller' => function ($request) {
+        if (is_leap_year($request->attributes->get('year'))) {
+            return new Response('Yep, this is a leap yar!!');
+        }
+
+        return new Response('Nope, this is not a leap yar!!');
+    }
+)));
+
+
+//$routes->add('hello', new Route('/hello/{name}', array('name' => 'world')));
+//$routes->add('bye', new Route('bye'));
 
 return $routes;
--- web/front.php
+++ web/front.php
@@ -5,14 +5,19 @@
  * Date: 2018/3/20
  * Time: 17:07
  */
+require_once __DIR__ . '/../vendor/autoload.php';
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
-use Symfony\Component\Routing\Matcher\Dumper\PhpMatcherDumper;
 use Symfony\Component\Routing\Matcher\UrlMatcher;
 use Symfony\Component\Routing\RequestContext;
 
-require_once __DIR__ . '/../vendor/autoload.php';
+function render_template($request) {
+    extract($request->attributes->all(), EXTR_SKIP);
+    ob_start();
+    include sprintf(__DIR__.'/../src/page/%s.php', $_route);
+    return new Response(ob_get_clean());
+}
 
 $request  = Request::createFromGlobals();
 $routes = include __DIR__.'/../src/app.php';
@@ -22,10 +27,8 @@ $context->fromRequest($request);
 $matcher = new UrlMatcher($routes, $context);
 
 try {
-    extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
-    ob_start();
-    include sprintf(__DIR__.'/../src/page/%s.php', $_route);
-    $response = new Response(ob_get_clean());
+    $request->attributes->add($matcher->match($request->getPathInfo()));
+    $response = call_user_func($request->attributes->get('_controller'), $request);
 } catch (ResourceNotFoundException $exception) {
     $response = new Response('Not Found!!!', 404);
 } catch (Exception $exception) {
