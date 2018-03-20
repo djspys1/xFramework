c1179c5a387891e41c02611feeb82174587330cb
xiaopeng-PC\xiaopeng <972551702@qq.com>
2018-03-20 21:15:42 +0800

    新增路由组件以优化相应的路由

---
+++ src/app.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xiaopeng
+ * Date: 2018/3/20
+ * Time: 20:49
+ */
+
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouteCollection;
+
+$routes = new RouteCollection();
+$routes->add('hello', new Route('/hello/{name}'));
+$routes->add('bye', new Route('bye'));
+
+return $routes;
--- web/front.php
+++ web/front.php
@@ -7,24 +7,29 @@
  */
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+use Symfony\Component\Routing\Matcher\Dumper\PhpMatcherDumper;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\RequestContext;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $request  = Request::createFromGlobals();
+$routes = include __DIR__.'/../src/app.php';
 
-$map = [
-    '/hello' => 'hello',
-    '/bye'   => 'bye',
-];
+$context = new RequestContext();
+$context->fromRequest($request);
+$matcher = new UrlMatcher($routes, $context);
 
-$path = $request->getPathInfo();
-if (isset($map[$path])) {
+try {
+    extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
     ob_start();
-    extract($request->query->all(), EXTR_SKIP);
-    include sprintf(__DIR__.'/../src/page/%s.php', $map[$path]);
+    include sprintf(__DIR__.'/../src/page/%s.php', $_route);
     $response = new Response(ob_get_clean());
-} else {
+} catch (ResourceNotFoundException $exception) {
     $response = new Response('Not Found!!!', 404);
+} catch (Exception $exception) {
+    $response = new Response('An Error occurred', 500);
 }
 
 $response->send();
